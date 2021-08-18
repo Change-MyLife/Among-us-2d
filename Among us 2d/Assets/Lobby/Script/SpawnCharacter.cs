dbb3c2a1dac7ed2a2e909b47353bb2f4c6241028
SeoMinWoo-student <tjalsdn5869@naver.com>
2021-08-18 16:38:15 +0900

    2021 08 18 AmongUs2d
    1. 다른플레이어의 애니메이션 동작이 안되는거 픽스
    Photon Animator View 인스펙터 -> Synchronize Parameters의 변수값을 Discrete로 설정.
    Photon View 인스펙터 -> Observed Components 에 Photon Animator View를 추가.
    
    2. 플레이어가 생성되는 위치 0~9번까지 픽스
    변경 : 플레이어가 스폰 되는 위치는 0~4번까지 밖에 안되게 함.

--- Among us 2d/Assets/Lobby/Script/SpawnCharacter.cs
+++ Among us 2d/Assets/Lobby/Script/SpawnCharacter.cs
@@ -6,15 +6,15 @@ using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class SpawnCharacter : MonoBehaviourPunCallbacks
+public class SpawnCharacter : MonoBehaviourPunCallbacks /*IPunObservable*/
 {
     public GameObject PlayerPrefab;
     public GameObject[] spawnpoz;
 
-    private int index = 0;
+    private int index;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Spawn();
     }
@@ -22,11 +22,23 @@ public class SpawnCharacter : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
     }
 
     public void Spawn()
     {
-        PhotonNetwork.Instantiate(this.PlayerPrefab.name, spawnpoz[index++].transform.position, Quaternion.identity);
+        index = Random.Range(0, 5);
+        PhotonNetwork.Instantiate(this.PlayerPrefab.name, spawnpoz[index].transform.position, Quaternion.identity);
     }
+
+    /*public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting) stream.SendNext(index);
+        else index = (int)stream.ReceiveNext();
+    }*/
 }
