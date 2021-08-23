65dfad345852fa8be34ffa1dd9971ac5f09c44bc
SeoMinWoo-student <tjalsdn5869@naver.com>
2021-08-23 14:51:24 +0900

    2021 08 23 AmongUs2d
    1. 캐릭터와 노트북 상호작용
    2. use버튼과 노트북 상호작용
    3. 캐릭터 색상 선택창 컬러추가

---
+++ Among us 2d/Assets/Lobby/Script/LaptopTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaptopTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Button UseButton;
+
+    [SerializeField]
+    private Sprite[] sprites;
+
+    [SerializeField]
+    private GameObject UI;
+
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("노트북과 상호작용");
+        UseButton.image.overrideSprite = sprites[1];
+        UseButton.interactable = true;
+
+        UseButton.onClick.AddListener(UseButtonOnClick);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("노트북에서 빠져나갔습니다.");
+        UseButton.image.overrideSprite = sprites[0];
+        UseButton.interactable = false;
+    }
+
+    void UseButtonOnClick()
+    {
+        UI.SetActive(true);
+    }
+}
