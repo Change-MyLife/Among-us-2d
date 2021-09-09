defb058fc9605b0dd52a3feb8db5bbcad0ceac0d
SeoMinWoo-student <tjalsdn5869@naver.com>
2021-09-09 15:27:54 +0900

    2021 09 09 AmongUs2d
    버그
    1. 캐릭터 색상 선택 안됨 동기화 불가능

--- Among us 2d/Assets/Lobby/Script/CharacterColorUI.cs
+++ Among us 2d/Assets/Lobby/Script/CharacterColorUI.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class CharacterColorUI : MonoBehaviour
+public class CharacterColorUI : MonoBehaviourPunCallbacks/*, IPunObservable*/ /*, IPunInstantiateMagicCallback*/
 {
     [SerializeField]
     private Image CrewImage;
     [SerializeField]
+    private GameObject Crewimage;
+    [SerializeField]
     private Button[] ColorButtons;
 
-    private void OnEnable()
+    int PrevNumber = -1;
+
+    /*private void OnEnable()
     {
         // 캐릭터 정지
         CharacterMove.isMoveable = false;
-    }
+    }*/
 
     public void Close()
     {
@@ -25,6 +31,49 @@ public class CharacterColorUI : MonoBehaviour
 
     public void OnColorButton(int Number)
     {
-        SpawnCharacter.myPlayer.GetComponent<SpriteRenderer>().material.SetColor("_PlayerColor", PlayerColor.GetColor((EPlayerColor)Number));
+        //photonView.RPC("ChangeColor",RpcTarget.All,Number);
+        // 선택가능한 색상?
+        if (!ColorButtons[Number].transform.GetChild(0).gameObject.activeSelf)
+        {
+            // 자신의 캐릭터 색깔 변경
+            //SpawnCharacter.myPlayer.GetComponent<SpriteRenderer>().material.SetColor("_PlayerColor", PlayerColor.GetColor((EPlayerColor)Number));
+
+            // UI 캐릭터의 색상 변경
+            CrewImage.GetComponent<Image>().material.SetColor("_PlayerColor", PlayerColor.GetColor((EPlayerColor)Number));
+
+            // 다른사람은 선택 못하게 객체 활성화
+            ColorButtons[Number].transform.GetChild(0).gameObject.SetActive(true);
+
+            // 이전의 선택했던 색상 버튼 비활성화
+            if (!(PrevNumber == -1))
+            {
+                ColorButtons[PrevNumber].transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+
+        PrevNumber = Number;
     }
+
+    /*public void OnPhotonInstantiate(PhotonMessageInfo info)
+    {
+        info.Sender.TagObject = this.gameObject;
+    }*/
+
+    [PunRPC]
+    void ChangeColor(int Number)
+    {
+        
+    }
+
+    /*public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(ColorButtons);
+        }
+        else
+        {
+            ColorButtons = (Button[])stream.ReceiveNext();
+        }
+    }*/
 }
--- Among us 2d/Assets/Lobby/Script/SpawnCharacter.cs
+++ Among us 2d/Assets/Lobby/Script/SpawnCharacter.cs
@@ -6,23 +6,22 @@ using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class SpawnCharacter : MonoBehaviourPunCallbacks /*IPunObservable*/
+public class SpawnCharacter : MonoBehaviourPunCallbacks /*, IPunObservable*/
 {
     public GameObject PlayerPrefab;
     public GameObject[] spawnpoz;
 
     private int index;
 
-    static public GameObject myPlayer;
+    PhotonView PV;
+
+    //public GameObject myPlayer;
+    static public List<GameObject> myPlayer = new List<GameObject>();
 
     void Awake()
     {
         Spawn();
-    }
-
-    void Update()
-    {
-        
+        PV = GetComponent<PhotonView>();
     }
 
     public override void OnJoinedLobby()
@@ -33,12 +32,38 @@ public class SpawnCharacter : MonoBehaviourPunCallbacks /*IPunObservable*/
     public void Spawn()
     {
         index = Random.Range(0, 5);
-        myPlayer = PhotonNetwork.Instantiate(this.PlayerPrefab.name, spawnpoz[index].transform.position, Quaternion.identity);
+        myPlayer.Add(PhotonNetwork.Instantiate(this.PlayerPrefab.name, spawnpoz[index].transform.position, Quaternion.identity));
+        
+        myPlayer[0].GetComponent<SpriteRenderer>().material.SetColor("_PlayerColor", PlayerColor.GetColor((EPlayerColor)0));
+        //myPlayer.GetComponent<SpriteRenderer>().material.SetColor("_PlayerColor", PlayerColor.GetColor((EPlayerColor)PhotonNetwork.CurrentRoom.PlayerCount - 1));
+    }
+
+    [ContextMenu("정보")]
+    void Info()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            print("현재 방 인원수 : " + PhotonNetwork.CurrentRoom.PlayerCount);
+            print("현재 방 최대인원수 : " + PhotonNetwork.CurrentRoom.MaxPlayers);
+
+            string playerStr = "방에 있는 플레이어 목록 : ";
+            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            {
+                playerStr += PhotonNetwork.PlayerList[i].NickName + ", ";
+            }
+            print(playerStr);
+        }
     }
 
     /*public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if (stream.IsWriting) stream.SendNext(index);
-        else index = (int)stream.ReceiveNext();
+        if (stream.IsWriting)
+        {
+            stream.SendNext(myPlayer);
+        }
+        else
+        {
+            myPlayer = (GameObject)stream.ReceiveNext();
+        }
     }*/
 }
