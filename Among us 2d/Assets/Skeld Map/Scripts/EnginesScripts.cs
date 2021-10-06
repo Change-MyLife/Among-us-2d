548eab6915191abef0a7e5c3927b215a20eef8b6
SeoMinWoo-student <tjalsdn5869@naver.com>
2021-10-06 21:17:44 +0900

    2021 10 06 AmongUs2d
    1. 상부 엔진 애니메이션 추가함
    
    해야 할 일
    1. 맵 구현

---
+++ Among us 2d/Assets/Skeld Map/Scripts/EnginesScripts.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnginesScripts : MonoBehaviour
+{
+    public GameObject[] Steams;
+
+    public GameObject[] Sparks;
+    public Sprite[] SparkImg;
+
+    void Start()
+    {
+        foreach(var steam in Steams)
+        {
+            StartCoroutine(RandomSteam(steam));
+        }
+
+        foreach (var spark in Sparks)
+        {
+            StartCoroutine(RandomSpark(spark));
+        }
+    }
+
+    private IEnumerator RandomSteam(GameObject steam)
+    {
+        while (true)
+        {
+            float RandTime = Random.Range(0.5f, 1.5f);
+            yield return new WaitForSeconds(RandTime);
+            steam.SetActive(true);
+            yield return new WaitForSeconds(0.6f);
+            steam.SetActive(false);
+        }
+    }
+
+    private IEnumerator RandomSpark(GameObject spark)
+    {
+        while (true)
+        {
+            float RandTime = Random.Range(0.5f, 1.5f);
+            yield return new WaitForSeconds(RandTime);
+            spark.SetActive(true);
+
+            for (int i = 0; i < 7; i++)
+            {
+                yield return null;
+                spark.GetComponent<SpriteRenderer>().sprite = SparkImg[Random.Range(0, 14)];
+            }
+
+            yield return new WaitForSeconds(0.15f);
+            spark.SetActive(false);
+        }
+    }
+}
